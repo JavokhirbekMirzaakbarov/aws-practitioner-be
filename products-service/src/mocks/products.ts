0ab277a41e0c28f19b0f1f91e2750a261ca300ae
Javokhirbek Mirzaakbarov <javokhirbek_mirzaakbarov@epam.com>
2023-11-13 01:20:16 +0500

    Change mock data

--- products-service/src/mocks/products.ts
+++ products-service/src/mocks/products.ts
@@ -1,31 +1,31 @@
 export default [
   {
     id: 1,
-    name: "Fitness Thingamajig",
+    title: "Fitness Thingamajig",
     price: "25.99",
     description: "Phasellus eget fringilla justo.",
   },
   {
     id: 2,
-    name: "Kids Supplies",
+    title: "Kids Supplies",
     price: "158.99",
     description: "Quisque nec imperdiet lectus. Aliquam justo dolor",
   },
   {
     id: 3,
-    name: "Camping Tool",
+    title: "Camping Tool",
     price: "163.99",
     description: "In at pharetra ligula, quis convallis nisl.",
   },
   {
     id: 4,
-    name: "Engine Thingamajig",
+    title: "Engine Thingamajig",
     price: "50",
     description: "Curabitur tristique vehicula vestibulum. ",
   },
   {
     id: 5,
-    name: "Toy Gadget",
+    title: "Toy Gadget",
     price: "86",
     description:
       "Pellentesque dictum ante vitae tempor sodales. Phasellus sed egestas felis, id sagittis ipsum. ",
